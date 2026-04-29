8149e66700c0500436babcfb498fce1e9ac1877c
qazedcswx <134367804+qazedcswx@users.noreply.github.com>
2026-04-29 22:34:28 +0900

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        array <int , 32> bits ;
+        bits.fill(0) ;
+
+        int res = 0 ;
+        for(int num : nums){
+            for(int i = 0 ; i < 32 ; ++i){
+                bits[i] += (num>>i) & 1 ; 
+            }
+        }
+
+        for(int i = 0 ; i < 32 ; ++i){
+            if(bits[i] > nums.size() / 2){
+                res |= (1<<i) ;
+            }
+        }
+
+        return res ;
+    }
+};
