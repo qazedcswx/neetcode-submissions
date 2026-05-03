ae4a79aeda6a0c2074f4e0806e5ec76c3fd8c8ed
qazedcswx <134367804+qazedcswx@users.noreply.github.com>
2026-05-03 11:25:49 +0900

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+//
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        unordered_map<int , int> num_map ;
+        long long tot = 1 ;
+        pair <int , int> zero = { 0 , 0 } ;
+        for( int i = 0 ; i < nums.size() ; ++i ){
+            if(nums[i] == 0) {
+                ++zero.first ;
+                zero.second = i ;
+                continue ;
+            }
+            tot *= nums[i] ;
+        }
+    //O(n)
+        
+        vector<int> output (nums.size() , 0) ;
+        if( zero.first >= 2 ) return output ;
+        if( zero.first == 1 ) {
+            output[zero.second] = tot ;
+            return output ;
+        }
+        for( int i = 0 ; i < nums.size() ; ++i ){
+            output[i] = tot / nums[i] ;
+        }
+        return output ;
+    }
+};
