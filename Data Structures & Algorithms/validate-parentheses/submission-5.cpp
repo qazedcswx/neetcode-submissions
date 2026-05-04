6c49abbd914d6e78093f4eb3ca78e1c32dac40fb
qazedcswx <134367804+qazedcswx@users.noreply.github.com>
2026-05-04 23:31:41 +0900

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.empty()) return true ;
+
+        stack<char> string_stack ;
+
+        for(char i : s){
+            switch( i ) {
+                case '(' :{
+                    string_stack.push(i) ;
+                    break ;
+                }
+                case '{' :{
+                    string_stack.push(i) ;
+                    break ;
+                }
+                case '[' :{
+                    string_stack.push(i) ;
+                    break ;
+                }
+                case ')' :{
+                    if(string_stack.empty() || string_stack.top() != '(') return false ;
+                    string_stack.pop() ;
+                    break ;
+                }
+                case '}' :{
+                    if(string_stack.empty() || string_stack.top() != '{') return false ;
+                    string_stack.pop() ;
+                    break ;
+                }
+                case ']' :{
+                    if(string_stack.empty() || string_stack.top() != '[') return false ;
+                    string_stack.pop() ;
+                    break ;
+                }
+            }
+        }
+        if(string_stack.empty()) return true ;
+        return false ;
+    }
+};
