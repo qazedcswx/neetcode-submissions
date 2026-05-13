a2aef03180a6b9b782c237fa3a1e233663a1b50d
qazedcswx <134367804+qazedcswx@users.noreply.github.com>
2026-05-13 11:25:40 +0900

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(std::vector<char>& s) {
+        if (s.empty()) return;
+
+        auto ptr1 = s.begin();
+        auto ptr2 = s.end() - 1; 
+
+        while (ptr1 < ptr2) {
+            *ptr1 ^= *ptr2 ^= *ptr1 ^= *ptr2 ;
+
+            ++ptr1;
+            --ptr2;
+        }
+    }
+};
